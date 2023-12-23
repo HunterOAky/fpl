e979856f12fc0ad413c87f6b42e4193bfaee9bc8
okoroha <okoroha@coventry.ac.uk>
2023-12-23 00:59:57 +0000

    revert

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 
 
 export const Banner = () => {
   return (
-    <Toolbar style={{ backgroundColor: "#37003c", width: "100%", textAlign:"center", marginTop:"0"}}>
-      <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-      </IconButton>
-      <h2 style={{ textAlign: "center", margin: "0 auto", width: "100%" }}>J F(antasy) K League Table 2023/24</h2>
-    </Toolbar>
+    <div className='title-container'>
+      <h2 className='title'>J F(antasy) K Leauge Table 2023/24</h2>
+    </div>
   )
 }
