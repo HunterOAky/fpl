eb39639ffe06c3f70d0b8047bf3343ab7034eff1
okoroha <okoroha@coventry.ac.uk>
2023-12-23 01:09:19 +0000

    nw build

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export const Banner = () => {
   return (
-    <div className='title-container'>
+    <div className='title-container' style={{marginTop:"5%"}}>
       <h2 className='title'>J F(antasy) K Leauge Table 2023/24</h2>
     </div>
   )
