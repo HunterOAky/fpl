eadc5af5cc5fcaeb71a86cd42a0b4586850b8c0a
okoroha <okoroha@coventry.ac.uk>
2023-12-24 13:28:32 +0000

    liam

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Banner = () => {
   return (
     <div className='title-container' style={{marginTop:"5%"}}>
-      <h2 className='title'>J F(antasy) K Leauge Table 2023/24</h2>
+      <h2 className='title'>J F(antasy) K Liam Table 2023/24</h2>
     </div>
   )
 }
